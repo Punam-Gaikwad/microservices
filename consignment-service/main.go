d09cc90489fa0156763da91acf0e9a44d975b6c1
Punam Gaikwad <punam.gaikwad@synerzip.com>
2019-09-09 12:24:42 +0530

    some changes

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -1,14 +1,12 @@
-// shippy-service-consignment/main.go
 package main
 
 import (
-	"context"
 	"log"
 	"net"
-	"sync"
 
-	//import generated protobuf code
+	// Import the generated protobuf code
 	pb "github.com/Punam-Gaikwad/microservices/consignment-service/proto/consignment"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -19,26 +17,29 @@ const (
 
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
+	GetAll() []*pb.Consignment
 }
 
 // Repository - Dummy repository, this simulates the use of a datastore
 // of some kind. We'll replace this with a real implementation later on.
 type Repository struct {
-	mu sync.RWMutex
 	consignments []*pb.Consignment
 }
 
 func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	repo.mu.Lock()
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
-	repo.mu.Unlock()
-
-	return consignment, nil;
+	return consignment, nil
 }
 
-// Repository - Dummy repository, this simulates the use of a datastore
-// of some kind. We'll replace this with a real implementation later on.
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
 type service struct {
 	repo repository
 }
@@ -47,7 +48,8 @@ type service struct {
 // which is a create method, which takes a context and a request as an
 // argument, these are handled by the gRPC server.
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
-	//save our consignment
+
+	// Save our consignment
 	consignment, err := s.repo.Create(req)
 	if err != nil {
 		return nil, err
@@ -58,23 +60,30 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 	return &pb.Response{Created: true, Consignment: consignment}, nil
 }
 
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignments: consignments}, nil
+}
+
 func main() {
+
 	repo := &Repository{}
 
 	// Set-up our gRPC server.
-	_, err := net.Listen("tcp", port)
+	lis, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatalf("failed to listen %v", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
-
-	s := grpc.NewServer() 
+	s := grpc.NewServer()
 
 	// Register our service with the gRPC server, this will tie our
 	// implementation into the auto-generated interface code for our
 	// protobuf definition.
-	pb.RegisterShipingServiceServer(s, &service{repo})
+	pb.RegisterShippingServiceServer(s, &service{repo})
 
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
-	log.Println("Running on port:", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
--- consignment-service/proto/consignment/consignment.pb.go
+++ consignment-service/proto/consignment/consignment.pb.go
@@ -158,19 +158,51 @@ func (m *Container) GetUserId() string {
 	return ""
 }
 
+type GetRequest struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *GetRequest) Reset()         { *m = GetRequest{} }
+func (m *GetRequest) String() string { return proto.CompactTextString(m) }
+func (*GetRequest) ProtoMessage()    {}
+func (*GetRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_e5e5ab05dfa973d5, []int{2}
+}
+
+func (m *GetRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_GetRequest.Unmarshal(m, b)
+}
+func (m *GetRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_GetRequest.Marshal(b, m, deterministic)
+}
+func (m *GetRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GetRequest.Merge(m, src)
+}
+func (m *GetRequest) XXX_Size() int {
+	return xxx_messageInfo_GetRequest.Size(m)
+}
+func (m *GetRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_GetRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_GetRequest proto.InternalMessageInfo
+
 type Response struct {
-	Created              bool         `protobuf:"varint,1,opt,name=created,proto3" json:"created,omitempty"`
-	Consignment          *Consignment `protobuf:"bytes,2,opt,name=consignment,proto3" json:"consignment,omitempty"`
-	XXX_NoUnkeyedLiteral struct{}     `json:"-"`
-	XXX_unrecognized     []byte       `json:"-"`
-	XXX_sizecache        int32        `json:"-"`
+	Created              bool           `protobuf:"varint,1,opt,name=created,proto3" json:"created,omitempty"`
+	Consignment          *Consignment   `protobuf:"bytes,2,opt,name=consignment,proto3" json:"consignment,omitempty"`
+	Consignments         []*Consignment `protobuf:"bytes,3,rep,name=consignments,proto3" json:"consignments,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}       `json:"-"`
+	XXX_unrecognized     []byte         `json:"-"`
+	XXX_sizecache        int32          `json:"-"`
 }
 
 func (m *Response) Reset()         { *m = Response{} }
 func (m *Response) String() string { return proto.CompactTextString(m) }
 func (*Response) ProtoMessage()    {}
 func (*Response) Descriptor() ([]byte, []int) {
-	return fileDescriptor_e5e5ab05dfa973d5, []int{2}
+	return fileDescriptor_e5e5ab05dfa973d5, []int{3}
 }
 
 func (m *Response) XXX_Unmarshal(b []byte) error {
@@ -205,9 +237,17 @@ func (m *Response) GetConsignment() *Consignment {
 	return nil
 }
 
+func (m *Response) GetConsignments() []*Consignment {
+	if m != nil {
+		return m.Consignments
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*Consignment)(nil), "consignment.Consignment")
 	proto.RegisterType((*Container)(nil), "consignment.Container")
+	proto.RegisterType((*GetRequest)(nil), "consignment.GetRequest")
 	proto.RegisterType((*Response)(nil), "consignment.Response")
 }
 
@@ -216,26 +256,29 @@ func init() {
 }
 
 var fileDescriptor_e5e5ab05dfa973d5 = []byte{
-	// 295 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x51, 0x4f, 0x4b, 0x3b, 0x31,
-	0x10, 0xfd, 0x6d, 0xff, 0xef, 0x04, 0x0a, 0xbf, 0x80, 0x35, 0xe8, 0xc1, 0x65, 0xbd, 0xf4, 0x54,
-	0xa1, 0x82, 0x07, 0xaf, 0x05, 0xa1, 0xd7, 0xf4, 0xe2, 0x4d, 0x6b, 0x32, 0x6c, 0x07, 0x6c, 0xb2,
-	0x24, 0x69, 0xfd, 0x52, 0x7e, 0x48, 0xd9, 0x6c, 0x57, 0xa3, 0xe2, 0x6d, 0xde, 0x7b, 0x33, 0xf3,
-	0x5e, 0x32, 0x70, 0x5d, 0x3b, 0x1b, 0xec, 0x8d, 0xb2, 0xc6, 0x53, 0x65, 0xf6, 0x68, 0x42, 0x5a,
-	0x2f, 0xa2, 0xca, 0x59, 0x42, 0x95, 0xef, 0x19, 0xb0, 0xd5, 0x17, 0xe6, 0x53, 0xe8, 0x91, 0x16,
-	0x59, 0x91, 0xcd, 0x73, 0xd9, 0x23, 0xcd, 0x0b, 0x60, 0x1a, 0xbd, 0x72, 0x54, 0x07, 0xb2, 0x46,
-	0xf4, 0xa2, 0x90, 0x52, 0x7c, 0x06, 0xa3, 0x37, 0xa4, 0x6a, 0x17, 0x44, 0xbf, 0xc8, 0xe6, 0x43,
-	0x79, 0x42, 0xfc, 0x0e, 0x40, 0x59, 0x13, 0xb6, 0x64, 0xd0, 0x79, 0x31, 0x28, 0xfa, 0x73, 0xb6,
-	0x9c, 0x2d, 0xd2, 0x38, 0xab, 0x4e, 0x96, 0x49, 0x27, 0xbf, 0x84, 0xfc, 0x88, 0xde, 0xe3, 0xeb,
-	0x13, 0x69, 0x31, 0x8c, 0x7e, 0x93, 0x96, 0x58, 0xeb, 0x72, 0x0f, 0xf9, 0xe7, 0xd4, 0xaf, 0xac,
-	0x57, 0xc0, 0xd4, 0xc1, 0x07, 0xbb, 0x47, 0xd7, 0xcc, 0xb6, 0x59, 0xa1, 0xa3, 0xd6, 0xba, 0x89,
-	0x6a, 0x1d, 0x55, 0x64, 0x62, 0xd4, 0x5c, 0x9e, 0x10, 0x3f, 0x87, 0xf1, 0xc1, 0xb7, 0x43, 0x83,
-	0x56, 0x68, 0xe0, 0x5a, 0x97, 0xcf, 0x30, 0x91, 0xe8, 0x6b, 0x6b, 0x3c, 0x72, 0x01, 0x63, 0xe5,
-	0x70, 0x1b, 0xb0, 0xb5, 0x9c, 0xc8, 0x0e, 0xf2, 0x7b, 0x48, 0xbf, 0x34, 0xfa, 0xb2, 0xa5, 0xf8,
-	0xf9, 0xd4, 0xae, 0x96, 0x69, 0xf3, 0xf2, 0x11, 0xa6, 0x9b, 0x1d, 0xd5, 0x64, 0xaa, 0x0d, 0xba,
-	0x23, 0x29, 0xe4, 0x0f, 0xf0, 0x7f, 0x15, 0x17, 0xa7, 0x67, 0xf9, 0x73, 0xdb, 0xc5, 0xd9, 0x37,
-	0xa5, 0x4b, 0x5b, 0xfe, 0x7b, 0x19, 0xc5, 0x6b, 0xdf, 0x7e, 0x04, 0x00, 0x00, 0xff, 0xff, 0x22,
-	0x3c, 0x09, 0x6c, 0x14, 0x02, 0x00, 0x00,
+	// 339 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x92, 0xcf, 0x4e, 0xc2, 0x40,
+	0x10, 0xc6, 0x2d, 0xff, 0x3b, 0x25, 0x12, 0x37, 0x11, 0x36, 0x7a, 0xb0, 0xa9, 0x17, 0x4e, 0x98,
+	0x60, 0xe2, 0xc1, 0x78, 0x6b, 0x22, 0xe1, 0xba, 0x3c, 0x80, 0xc1, 0x76, 0x52, 0x26, 0x91, 0xdd,
+	0xba, 0xbb, 0xe0, 0xdb, 0xf0, 0x04, 0x3e, 0xa4, 0x61, 0x4b, 0x65, 0xd1, 0x70, 0xdb, 0x6f, 0xbe,
+	0x6f, 0xb6, 0xbf, 0x99, 0x2e, 0xdc, 0x97, 0x5a, 0x59, 0xf5, 0x90, 0x29, 0x69, 0xa8, 0x90, 0x6b,
+	0x94, 0xd6, 0x3f, 0x4f, 0x9c, 0xcb, 0x22, 0xaf, 0x94, 0x7c, 0x07, 0x10, 0xa5, 0x47, 0xcd, 0x2e,
+	0xa1, 0x41, 0x39, 0x0f, 0xe2, 0x60, 0x1c, 0x8a, 0x06, 0xe5, 0x2c, 0x86, 0x28, 0x47, 0x93, 0x69,
+	0x2a, 0x2d, 0x29, 0xc9, 0x1b, 0xce, 0xf0, 0x4b, 0x6c, 0x08, 0x9d, 0x2f, 0xa4, 0x62, 0x65, 0x79,
+	0x33, 0x0e, 0xc6, 0x6d, 0x71, 0x50, 0xec, 0x09, 0x20, 0x53, 0xd2, 0x2e, 0x49, 0xa2, 0x36, 0xbc,
+	0x15, 0x37, 0xc7, 0xd1, 0x74, 0x38, 0xf1, 0x71, 0xd2, 0xda, 0x16, 0x5e, 0x92, 0xdd, 0x42, 0xb8,
+	0x45, 0x63, 0xf0, 0xe3, 0x8d, 0x72, 0xde, 0x76, 0xdf, 0xeb, 0x55, 0x85, 0x79, 0x9e, 0xac, 0x21,
+	0xfc, 0xed, 0xfa, 0xc7, 0x7a, 0x07, 0x51, 0xb6, 0x31, 0x56, 0xad, 0x51, 0xef, 0x7b, 0x2b, 0x56,
+	0xa8, 0x4b, 0xf3, 0x7c, 0x8f, 0xaa, 0x34, 0x15, 0x24, 0x1d, 0x6a, 0x28, 0x0e, 0x8a, 0x8d, 0xa0,
+	0xbb, 0x31, 0x55, 0x53, 0xab, 0x32, 0xf6, 0x72, 0x9e, 0x27, 0x7d, 0x80, 0x19, 0x5a, 0x81, 0x9f,
+	0x1b, 0x34, 0x36, 0xd9, 0x05, 0xd0, 0x13, 0x68, 0x4a, 0x25, 0x0d, 0x32, 0x0e, 0xdd, 0x4c, 0xe3,
+	0xd2, 0x62, 0x45, 0xd0, 0x13, 0xb5, 0x64, 0xcf, 0xe0, 0x6f, 0xd8, 0x61, 0x44, 0x53, 0xfe, 0x77,
+	0xf2, 0xfa, 0x2c, 0xfc, 0x30, 0x7b, 0x81, 0xbe, 0x27, 0x0d, 0x6f, 0xba, 0xb5, 0x9d, 0x6f, 0x3e,
+	0x49, 0x4f, 0x77, 0x01, 0x0c, 0x16, 0x2b, 0x2a, 0x4b, 0x92, 0xc5, 0x02, 0xf5, 0x96, 0x32, 0x64,
+	0xaf, 0x70, 0x95, 0x3a, 0x30, 0xff, 0x2f, 0x9f, 0xbd, 0xf0, 0xe6, 0xfa, 0xc4, 0xa9, 0xa7, 0x4d,
+	0x2e, 0x58, 0x0a, 0x83, 0x19, 0x5a, 0x2f, 0x6a, 0xd8, 0xe8, 0x24, 0x7b, 0x5c, 0xd4, 0xd9, 0x4b,
+	0xde, 0x3b, 0xee, 0x05, 0x3e, 0xfe, 0x04, 0x00, 0x00, 0xff, 0xff, 0x65, 0x3e, 0x9a, 0xa6, 0xa8,
+	0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -246,72 +289,108 @@ var _ grpc.ClientConn
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion4
 
-// ShipingServiceClient is the client API for ShipingService service.
+// ShippingServiceClient is the client API for ShippingService service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type ShipingServiceClient interface {
+type ShippingServiceClient interface {
 	CreateConsignment(ctx context.Context, in *Consignment, opts ...grpc.CallOption) (*Response, error)
+	GetConsignments(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*Response, error)
 }
 
-type shipingServiceClient struct {
+type shippingServiceClient struct {
 	cc *grpc.ClientConn
 }
 
-func NewShipingServiceClient(cc *grpc.ClientConn) ShipingServiceClient {
-	return &shipingServiceClient{cc}
+func NewShippingServiceClient(cc *grpc.ClientConn) ShippingServiceClient {
+	return &shippingServiceClient{cc}
 }
 
-func (c *shipingServiceClient) CreateConsignment(ctx context.Context, in *Consignment, opts ...grpc.CallOption) (*Response, error) {
+func (c *shippingServiceClient) CreateConsignment(ctx context.Context, in *Consignment, opts ...grpc.CallOption) (*Response, error) {
 	out := new(Response)
-	err := c.cc.Invoke(ctx, "/consignment.ShipingService/CreateConsignment", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/consignment.ShippingService/CreateConsignment", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// ShipingServiceServer is the server API for ShipingService service.
-type ShipingServiceServer interface {
+func (c *shippingServiceClient) GetConsignments(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*Response, error) {
+	out := new(Response)
+	err := c.cc.Invoke(ctx, "/consignment.ShippingService/GetConsignments", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+// ShippingServiceServer is the server API for ShippingService service.
+type ShippingServiceServer interface {
 	CreateConsignment(context.Context, *Consignment) (*Response, error)
+	GetConsignments(context.Context, *GetRequest) (*Response, error)
 }
 
-// UnimplementedShipingServiceServer can be embedded to have forward compatible implementations.
-type UnimplementedShipingServiceServer struct {
+// UnimplementedShippingServiceServer can be embedded to have forward compatible implementations.
+type UnimplementedShippingServiceServer struct {
 }
 
-func (*UnimplementedShipingServiceServer) CreateConsignment(ctx context.Context, req *Consignment) (*Response, error) {
+func (*UnimplementedShippingServiceServer) CreateConsignment(ctx context.Context, req *Consignment) (*Response, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateConsignment not implemented")
 }
-
-func RegisterShipingServiceServer(s *grpc.Server, srv ShipingServiceServer) {
-	s.RegisterService(&_ShipingService_serviceDesc, srv)
+func (*UnimplementedShippingServiceServer) GetConsignments(ctx context.Context, req *GetRequest) (*Response, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetConsignments not implemented")
 }
 
-func _ShipingService_CreateConsignment_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func RegisterShippingServiceServer(s *grpc.Server, srv ShippingServiceServer) {
+	s.RegisterService(&_ShippingService_serviceDesc, srv)
+}
+
+func _ShippingService_CreateConsignment_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Consignment)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(ShipingServiceServer).CreateConsignment(ctx, in)
+		return srv.(ShippingServiceServer).CreateConsignment(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/consignment.ShipingService/CreateConsignment",
+		FullMethod: "/consignment.ShippingService/CreateConsignment",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ShipingServiceServer).CreateConsignment(ctx, req.(*Consignment))
+		return srv.(ShippingServiceServer).CreateConsignment(ctx, req.(*Consignment))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-var _ShipingService_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "consignment.ShipingService",
-	HandlerType: (*ShipingServiceServer)(nil),
+func _ShippingService_GetConsignments_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GetRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ShippingServiceServer).GetConsignments(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/consignment.ShippingService/GetConsignments",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ShippingServiceServer).GetConsignments(ctx, req.(*GetRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+var _ShippingService_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "consignment.ShippingService",
+	HandlerType: (*ShippingServiceServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "CreateConsignment",
-			Handler:    _ShipingService_CreateConsignment_Handler,
+			Handler:    _ShippingService_CreateConsignment_Handler,
+		},
+		{
+			MethodName: "GetConsignments",
+			Handler:    _ShippingService_GetConsignments_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
